ce85c20c1d156cd163572386e94b73b09ee44889
MadhavaRahul <149048809+MadhavaRahul@users.noreply.github.com>
2024-08-25 13:57:40 +0530

    Add files via upload

---
+++ python_udemyexc/s_pOpt.py
@@ -0,0 +1,18 @@
+a=1
+b=5
+c=4
+
+""" sum 
+        of 
+            the 
+                roots """
+x= -b/(a)
+print(f'x1+x2= {x}')
+
+""" product 
+        of 
+            the 
+                roots """
+
+y= c/(a)
+print(f'x1*x2= {y}')
---
+++ python_udemyexc/sum_pro.py
@@ -0,0 +1,20 @@
+a=1
+b=5
+c=4
+#formula for two roots
+x1=(-b+(b**2-4*a*c)**0.5)/2*a
+x2=(-b-(b**2-4*a*c)**0.5)/2*a
+
+print(f'x1= {x1}') #1st root
+print(f'x2= {x2}') #2st root
+
+""" sum 
+        of 
+            the 
+                roots """
+print(f'x1+x2= {x1+x2}')
+""" product 
+        of 
+            the 
+                roots """
+print(f'x1*= {x1*x2}')
